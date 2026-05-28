f2abb4aa2396cdde2e1b1a95fca5b7d27e010459
Prashant Talan <prtalan@altimetrik.com>
2026-05-28 22:57:26 +0530

    added day 1

--- day 1.py
+++ day 1.py
@@ -24,10 +24,27 @@ int main() {
     scanf("%d", &n);
 
     printf("Multiplication Table of %d:\n", n);
-
     for(int i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", n, i, n * i);
     }
+    return 0;
+}
+
+q3.c
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("Factorial of %d = %lld", n, factorial);
 
     return 0;
 }

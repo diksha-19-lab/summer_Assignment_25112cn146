0bc6eed4d4eaf88f5f1d8a4275a42e1d80707f77
Prashant Talan <prtalan@altimetrik.com>
2026-05-28 22:46:11 +0530

    added day 1

--- day 1.py
+++ day 1.py
@@ -14,3 +14,20 @@ int main() {
 
     return 0;
 }
+q2.c
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Multiplication Table of %d:\n", n);
+
+    for(int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+
+    return 0;
+}

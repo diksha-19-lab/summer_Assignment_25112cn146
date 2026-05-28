e43509820c0317f9ec92f552c1f560944953f78c
Prashant Talan <prtalan@altimetrik.com>
2026-05-28 23:00:04 +0530

    added day 1

--- day 1.py
+++ day 1.py
@@ -48,3 +48,22 @@ int main() {
 
     return 0;
 }
+
+q4.c
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}

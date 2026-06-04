6470e8e587d84a9b269a51b589a683750a297f74
Prashant Talan <prtalan@altimetrik.com>
2026-06-04 17:24:03 +0530

    Added day3

---
+++ day3/day4/check armstrong number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,temp,sum=0;
+    cin >> n;
+    temp = n;
+    while( temp>0)
+    {
+        int d=temp%10;
+        sum+=d*d*d;
+        temp /=10;
+        
+    }
+    if(sum==n)
+    
+        cout << "Armstrong number";
+    
+    else
+    
+        cout << "Not an Armstrong number" ;
+        return 0;
+}
+    
